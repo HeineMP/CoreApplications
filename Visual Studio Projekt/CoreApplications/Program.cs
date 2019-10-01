012a1c9620d6b8a0a8394725ca07e3512a2d4349
Sebastian Kristensen <sebastian@z-designs.dk>
2019-10-01 12:49:47 +0200

    First usable actual code
    - Function for MySQL connection
    - Function to call create_customer_user in MySQL stored procedures
    - Function to execute MySQL query

--- Visual Studio Projekt/CoreApplications/Program.cs
+++ Visual Studio Projekt/CoreApplications/Program.cs
@@ -7,7 +7,35 @@ namespace CoreApplications
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            CreateCustomerUser("Jens","Jensen",1,"+4521212121","lort@wow.dk");
         }
+
+        public static void CreateCustomerUser(string fname, string lname, int company, string phone, string email)
+        {
+            var query = $"CALL create_customer_user('{fname}','{lname}',{company},'{phone}','{email}')";
+            ExecuteQuery(query);
+        }
+
+        public static MySqlConnection Connection()
+        {
+            var connectionString = new MySqlConnectionStringBuilder();
+            connectionString.Server = "172.16.240.240";
+            connectionString.UserID = "svc_sqlconnect";
+            connectionString.Password = "St7XmYZQhzmNL4ZY";
+            connectionString.Database = "core_applications";
+
+            var connection = new MySqlConnection(connectionString.ConnectionString);
+            return connection;
+        }
+
+        public static void ExecuteQuery(string query)
+        {
+            var conn = Connection();
+            MySqlCommand cmd = new MySqlCommand(query, conn);
+            conn.Open();
+            cmd.ExecuteNonQuery();
+            conn.Close();
+        }
+
     }
 }
